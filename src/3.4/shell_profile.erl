7a91defe0f24d0fc8e7c1ddf18e3d81264de8196
Prakash Parmar <prakash.parmar@outlook.com>
2017-12-09 16:46:07 +0530

    Add Default Colours and Add/Modifies Functions to beautify.

---
+++ src/3.4/shell_profile.erl
@@ -0,0 +1,66 @@
+%%%-------------------------------------------------------------------
+%%% 
+%%% @author 	Prakash Parmar <prakash.parmar@outlook.com>
+%%%
+%%% @version 	Beta
+%%%
+%%% @doc
+%%%		Module Contains Default profile settings and API to beautify. 
+%%% @end 
+%%%
+%%% @copyright 2017 Prakash Parmar
+%%%-------------------------------------------------------------------
+
+-module(shell_profile).
+
+%% ====================================================================
+%% API functions
+%% ====================================================================
+-export([
+			beautify/2 
+		]).
+
+%% ====================================================================
+%% Definitions
+%% ====================================================================
+
+-define( DEFAULT_PROFILE,	[
+								{ sb, 	"\e[0;37m~ts\e[0m" }, 	% $[]
+								
+								{ cb, 	"\e[0;34m~ts\e[0m" }, 	% ${}
+
+								{ bstr,	"\e[0;34m~ts\e[0m" },	% $<< >>
+
+								{ pipe,		"\e[0;34m~ts\e[0m" }, 	% $|
+								{ comma,	"\e[0;34m~ts\e[0m" }, 	% $,
+								{ colon,	"\e[0;34m~ts\e[0m" },	% $:
+								{ hash,		"\e[0;34m~ts\e[0m" },	% $#
+								{ ellipsis, "\e[0;34m~ts\e[0m" },   % $...
+								{ eq,		"\e[0;34m~ts\e[0m" },	% =
+								{ arrow,	"\e[0;34m~ts\e[0m" },	% =>
+								
+								{ term, 	"\e[0;36m~ts\e[0m" },
+								{ name, 	"\e[0;35m~ts\e[0m" },
+								{ field,	"\e[0;34m~ts\e[0m" },
+								{ string,	"\e[0;33m~ts\e[0m" },
+								{ digits,	"\e[0;32m~ts\e[0m" },
+								{ function, "\e[0;35m~ts\e[0m" }
+
+		 					]).
+
+%% ====================================================================
+%% APIs
+%% ====================================================================
+
+beautify(Class,Str) ->
+	case lists:keyfind(Class, 1, ?DEFAULT_PROFILE) of
+		{ Class, Format_specifier } ->
+			lists:flatten(io_lib:format(Format_specifier,[Str]));
+		_not_found ->
+			Str
+	end.
+
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
+
